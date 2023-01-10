891faf65a0d0a41168349ae487f15497e5cb5867
Evan Cook <cook.evank@gmail.com>
2023-01-10 10:43:27 -0500

    add home resume things

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,6 +1,8 @@
+import "./Button.css";
+
 function Button(props) {
   return (
-    <a href={props.link}>
+    <a href={props.link} className="button" target="_blank" rel="noreferrer">
       <span>{props.text}</span>
     </a>
   );
