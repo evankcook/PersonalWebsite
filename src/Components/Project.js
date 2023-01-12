fae53af6817ae1eebb573e3deae14010377260da
Evan Cook <cook.evank@gmail.com>
2023-01-11 20:45:05 -0500

    refactor code in project

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -2,33 +2,27 @@ import Button from "./Button";
 import "./Project.css";
 
 function Project(props) {
-  if (props.isImageLeft) {
-    return (
-      <div className="content-container">
-        <div className="image-box">
-          <img src={props.imgPath} alt={props.alt} id="project-img-left" />
-        </div>
-        <div className="text-box-left">
-          <h3>{props.title}</h3>
-          <p>{props.description}</p>
-          <Button link={props.link} text="Check it out" />
-        </div>
-      </div>
-    );
-  } else {
-    return (
-      <div className="content-container">
+  return (
+    <div className="content-container">
+      {props.isImageLeft || (
         <div className="text-box-right">
           <h3>{props.title}</h3>
           <p>{props.description} hi</p>
           <Button link={props.link} text="Check it out" />
         </div>
-        <div className="image-box">
-          <img src={props.imgPath} alt={props.alt} id="project-img-right" />
-        </div>
+      )}
+      <div className="image-box">
+        <img src={props.imgPath} alt={props.alt} id="project-img-left" />
       </div>
-    );
-  }
+      {props.isImageLeft && (
+        <div className="text-box-left">
+          <h3>{props.title}</h3>
+          <p>{props.description}</p>
+          <Button link={props.link} text="Check it out" />
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Project;
