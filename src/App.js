2e28d7d4e8dcee0a94da859706f3b464d09e5765
Evan Cook <cook.evank@gmail.com>
2023-02-02 19:12:54 -0500

    add descriptions

--- src/App.js
+++ src/App.js
@@ -85,12 +85,7 @@ function App() {
         <h3>
           Computer science and business student at Northeastern University
         </h3>
-        <p className="resume-text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-        </p>
+        <p className="resume-text"></p>
         <Button
           link="./Resume_ Evan Kosei Cook_1_26.pdf"
           text="Check out my resume"
@@ -98,18 +93,7 @@ function App() {
       </div>
       <Section id="about-me" title="About Me">
         <div className="content-container">
-          <p className="about-me-text">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-            quae ab illo inventore veritatis et quasi architecto beatae vitae
-            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-            eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
-            qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-            sed quia non numquam eius modi tempora incidunt ut labore et dolore
-            magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
-            nostrum exercitationem
-          </p>
+          <p className="about-me-text"></p>
           <img
             src="images/personal-photo.svg"
             alt="Evan Kosei Cook"
